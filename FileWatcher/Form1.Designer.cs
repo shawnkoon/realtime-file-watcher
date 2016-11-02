f5845d532e2caf9a00bf290c5b868245c045a345
shawnkoon <shawn9405@gmail.com>
2016-11-02 14:42:07 -0700

    Added Basic components on the form

--- FileWatcher/Form1.Designer.cs
+++ FileWatcher/Form1.Designer.cs
@@ -29,29 +29,105 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.startWatchingButton = new System.Windows.Forms.Button();
+            this.resultListBox = new System.Windows.Forms.ListBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(395, 67);
             this.label1.TabIndex = 0;
             this.label1.Text = "File Watcher :)";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FileWatcher.Properties.Resources.Deal_With_It;
+            this.pictureBox1.Location = new System.Drawing.Point(412, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openFileButton.Location = new System.Drawing.Point(12, 92);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(163, 91);
+            this.openFileButton.TabIndex = 2;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            // 
+            // startWatchingButton
+            // 
+            this.startWatchingButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startWatchingButton.Location = new System.Drawing.Point(328, 92);
+            this.startWatchingButton.Name = "startWatchingButton";
+            this.startWatchingButton.Size = new System.Drawing.Size(184, 91);
+            this.startWatchingButton.TabIndex = 3;
+            this.startWatchingButton.Text = "Start Watching";
+            this.startWatchingButton.UseVisualStyleBackColor = true;
+            // 
+            // resultListBox
+            // 
+            this.resultListBox.FormattingEnabled = true;
+            this.resultListBox.Location = new System.Drawing.Point(12, 201);
+            this.resultListBox.Name = "resultListBox";
+            this.resultListBox.Size = new System.Drawing.Size(500, 225);
+            this.resultListBox.TabIndex = 4;
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.LightCoral;
+            this.clearButton.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.clearButton.Location = new System.Drawing.Point(198, 140);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(108, 49);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(198, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "About";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(524, 431);
+            this.ClientSize = new System.Drawing.Size(524, 436);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.resultListBox);
+            this.Controls.Add(this.startWatchingButton);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "File Watcher!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,6 +136,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button startWatchingButton;
+        private System.Windows.Forms.ListBox resultListBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
